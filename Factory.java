d580557b0c10abae3f18fc21f7456ea3e646b146
JPOrellana <115191781+JPOrellana@users.noreply.github.com>
2023-04-21 17:40:58 -0600

    Clase Factory y Translator

--- Factory.java
+++ Factory.java
@@ -1,3 +1,19 @@
-public class Factory {
-    
+/**
+ * @author Pablo Orellana
+ * Clase Fsctory
+ */
+
+public class Factory<K extends Comparable<K>,V>{
+	/**
+	 * Haber elegido un traductor 
+	 * Luego intancia de tipo de tranductor seleccionado
+	 * @param tipoTranslator Tipo de translator a instanciar
+	 */
+	public TranslatorInterface<K, V> getTranslator(int tipoTranslator){
+		if (tipoTranslator == 1) { 
+			return new Hashing<K,V>();
+		} else { 
+			return new MapSplayTree<K,V>();
+		}
+	}
 }
